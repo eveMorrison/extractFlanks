a4cea1a023530c48b6eac1a80979306c21971240
Eva Morrison <eva.morrison33@ku.edu>
2021-04-19 17:44:30 -0500

    change where the files are opened and closed

--- extract.py
+++ extract.py
@@ -5,34 +5,52 @@ def getTransposonPos(f):
         res = x.split()
     return (res[5],res[6])
 
-def getSequence(fasta,start,end):
+def getSequence(start,end):
+    fasta = open("both500.fa.out_both500_flank_500.fasta")
     seq = ""
     for x in fasta:
         res = x.split()
     for x in range(start,end):
         seq += (res[0][x])
+    fasta.close()
     return (seq)
 
-def getRightFlankPos(fasta):
-    res = fasta.readline()
-    res = res.split()
-        #^.+[|(](\d+)[.]+(\d+)
-    print(res)
+def getSequence1(start,end):
+    fasta = open("both500.fa.out_both500_flank_500.fasta")
+    seq = ""
+    for x in fasta:
+        txt = x
+        txt = txt.split()
+    for x in range(start,end):
+        seq = seq + (txt[0][x])
+    fasta.close()
+    return (seq)
+
+def getFlankPos():
+    fasta = open("both500.fa.out_both500_flank_500.fasta")
+    txt = fasta.readline()
+    txt = txt.split()
+    num = re.search('^.+[|(](\d+)[.]+(\d+)',txt[0],0)
+    flankBegin = int(num.group(1))
+    flankEnd = int(num.group(2))
+    fasta.close()
+    return(flankBegin,flankEnd)
 
 
-print("hello world")
 f = open("both500.fa.out")
-fasta = open("both500.fa.out_both500_flank_500.fasta")
 
-getRightFlankPos(fasta)
+
+flankPos = getFlankPos()
+leftFlankStart = int(flankPos[0])
+rightFlankEnd = int(flankPos[1])
 
 location = getTransposonPos(f)
-start = int(location[0])
-end = int(location[1])
+leftFlankEnd = int(location[0])
+rightFlankStart = int(location[1])
 
-transposon = getSequence(fasta,start,end)
+transposon = getSequence(leftFlankEnd,rightFlankStart)
 print(transposon)
-
-length = int(end) - int(start)
-
-print(length)
+flankLeft = getSequence(leftFlankStart,leftFlankEnd)
+print(flankLeft)
+flankRight = getSequence(rightFlankStart,rightFlankEnd )
+print(flankRight)

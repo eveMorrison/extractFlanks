df92f76edea0e5457fe6c019d7bd96f1df850ad9
Eva Morrison <eva.morrison33@ku.edu>
2021-04-19 12:48:59 -0500

    get right flank start position

--- extract.py
+++ extract.py
@@ -1,4 +1,6 @@
-def getPos(f):
+import re
+
+def getTransposonPos(f):
     for x in f:
         res = x.split()
     return (res[5],res[6])
@@ -11,12 +13,20 @@ def getSequence(fasta,start,end):
         seq += (res[0][x])
     return (seq)
 
+def getRightFlankPos(fasta):
+    res = fasta.readline()
+    res = res.split()
+        #^.+[|(](\d+)[.]+(\d+)
+    print(res)
+
 
 print("hello world")
 f = open("both500.fa.out")
 fasta = open("both500.fa.out_both500_flank_500.fasta")
 
-location = getPos(f)
+getRightFlankPos(fasta)
+
+location = getTransposonPos(f)
 start = int(location[0])
 end = int(location[1])
 

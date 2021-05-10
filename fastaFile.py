dbb84d018a172d14e2d7e033054ac65aef05fc1e
Eva Morrison <eva.morrison33@ku.edu>
2021-05-10 11:26:14 -0500

    iterate through files

---
+++ fastaFile.py
@@ -0,0 +1,37 @@
+"""Parse a FASTA file."""
+
+from pprint import pprint
+
+fasta_file = "both500.fa.out_both500_flank_500.fasta"
+
+def parse_fasta(fname):
+    with open(fname, "r") as fh:
+
+        # Create variables for storing the identifiers and the sequence.
+        identifier = None
+        sequence = []
+
+        for line in fh:
+            line = line.strip()  # Remove trailing newline characters.
+            if line.startswith(">"):
+                if identifier is None:
+                    # This only happens when the first line of the
+                    # FASTA file is parsed.
+                    identifier = line
+                else:
+                    # This happens every time a new FASTA record is
+                    # encountered.
+
+                    # Start by yielding the entry that has been built up.
+                    yield identifier, sequence
+
+                    # Then reinitialise the identifier and sequence
+                    # variables to build up a new record.
+                    identifier = line
+                    sequence = []
+            else:
+                # This happens every time a sequence line is encountered.
+                sequence.append(line)
+
+for entry in parse_fasta(fasta_file):
+    pprint(entry)
---
+++ fileIteration.py
@@ -0,0 +1,12 @@
+repMaskFile = open("both500.fa.out", "r")
+
+linelist = repMaskFile.readlines()
+fileLeng = len(linelist)
+
+i = 3
+while(i<fileLeng):
+   values = linelist[i].split()
+   print('ID: ',values[14], '\nTransposon Start: ', values[5], '\nTransposon End: ', values[6] )
+   i+=1
+ 
+repMaskFile.close()

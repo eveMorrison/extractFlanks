c40203dd0699ceef289910d712590aad62db47bc
Eva Morrison <eva.morrison33@ku.edu>
2021-05-17 15:23:38 -0500

    add comments to program

--- extract.py
+++ extract.py
@@ -6,27 +6,9 @@ import re
 fasta_file = "test.fasta"
 repMask_file = "test.fa.out"
 
-def parse_fasta(fname):
-    with open(fname, "r") as fh:
 
-        # Create variables for storing the identifiers and the sequence.
-        identifier = None
-        sequence = []
-
-        for line in fh:
-            line = line.strip()  # Remove trailing newline characters.
-            if line.startswith(">"):
-                # Then reinitialise the identifier and sequence
-                # variables to build up a new record.
-                identifier = line
-                sequence = [] 
-            else:
-                # This happens every time a sequence line is encountered.
-                sequence.append(line)
-                # Start by yielding the entry that has been built up.
-                yield identifier, sequence        
-
-
+#find the corresponding fasta sequence to the repeatmasker output
+#One code to find them all is not sorted
 def find_fa_seq(fname,leftFlank,rightFlank):
     with open(fname, "r") as fh:
 
@@ -49,38 +31,39 @@ def find_fa_seq(fname,leftFlank,rightFlank):
             else:
                 # This happens every time a sequence line is encountered.
                 sequence.append(line)
+                #corresponding found transposon will be within one code to find them all positions
                 if(left_flank_begin < leftFlank and right_flank_end > rightFlank):
                     # Start by yielding the entry that has been built up.
                     yield identifier, sequence
 
+
+#find every line containing transposon information
 def parse_repMask(fname):
     with open(fname, "r") as fh:
 
         for line in fh:
             line = line.strip()
+            #lines begin with smith-waterman score
+            #look to see if the line begins with a number
+            #if line begins with number find the Repeat Masker ID number and transposon locations
             if re.search('^.+\d',line,0):
                 values = line.split()
                 id = values[14]
                 te_start = values[5]
                 te_end = values[6]
+                #return the Repeat Masker ID, transposon start position and transposon end position
                 yield id, te_start, te_end
 
-for entry in parse_fasta(fasta_file):
-    #pprint(entry)
-    num = re.search('^.+[|(](\d+)[.]+(\d+)',entry[0],0)
-    left_flank_begin = int(num.group(1))
-    right_flank_end = int(num.group(2))
-    #pprint(left_flank_begin)
-    #pprint(right_flank_end)
-
-
+#open the repeat masker file 
+#for every found transposon
+#then search for corresponding One Code to Find Them All fasta sequence
+#finally print the found sequences for the trasnposon and the flanks to their corresponding files
 for entry in parse_repMask(repMask_file):
     te_id = entry[0]
     left_flank_end = int(entry[1])
     right_flank_begin = int(entry[2])
-    #print("ID: " + te_id)
-    #print("Left flank end: " + left_flank_end)
-    #print("Right flank start: " + right_flank_begin)
+
+    #find the corresponding One Code to Find Them All sequence
     for coordinate in find_fa_seq(fasta_file,left_flank_end,right_flank_begin):
         num = re.search('^.+[|(](\d+)[.]+(\d+)',coordinate[0],0)
         left_flank_begin = int(num.group(1))
@@ -95,19 +78,26 @@ for entry in parse_repMask(repMask_file):
         left_flank_length = left_flank_end - left_flank_begin
         right_flank_length = right_flank_end - right_flank_begin
 
-        #pprint(coordinate)
 
         #extract the flanking sequences
+
+        #append transposon to TE fasta file
         transposon = coordinate[1][0][te_start : te_end+1]
         f = open("transposon.fasta", "a")
         f.write(">" + te_id + "\n")
         f.write(transposon + "\n")
         f.close()
+
+        #append flanking sequence to left flank fasta file
+        #get sequence from beginning of string to beginning of transposon
         flankLeft = coordinate[1][0][ : te_start]
         f = open("leftFlanks.fasta", "a")
         f.write(">" + te_id + "\n")
         f.write(flankLeft + "\n")
         f.close()
+
+        #append flanking sequence to right flank fasta file
+        #get sequence from beginning of transposon to end of the string
         flankRight = coordinate[1][0][te_end+1: ]
         f = open("rightFlanks.fasta", "a")
         f.write(">" + te_id + "\n")

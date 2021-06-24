b94df9e627441712a3584b738e08984402744c32
Eva Morrison <eva.morrison33@ku.edu>
2021-06-24 17:07:39 -0500

    correct python file to find matching sequence ID

--- extract.py
+++ extract.py
@@ -1,15 +1,13 @@
-"""Parse a FASTA file."""
-
 from pprint import pprint
 import re
 
-fasta_file = "test.fasta"
-repMask_file = "test.fa.out"
+fasta_file = "nondys_60000_filter.fa.out_d9908dde-db5b-4e12-b663-d4f1e55d17c2_flank_500.fasta"
+repMask_file = "nondys_60000_filter.fa.out"
 
 
 #find the corresponding fasta sequence to the repeatmasker output
 #One code to find them all is not sorted
-def find_fa_seq(fname,leftFlank,rightFlank):
+def find_fa_seq(fname,seqID,leftFlank,rightFlank):
     with open(fname, "r") as fh:
 
         # Create variables for storing the identifiers and the sequence.
@@ -17,6 +15,7 @@ def find_fa_seq(fname,leftFlank,rightFlank):
         sequence = []
         left_flank_begin = '\0'
         right_flank_end = '\0'
+        readID = ""
 
         for line in fh:
             line = line.strip()  # Remove trailing newline characters.
@@ -24,15 +23,17 @@ def find_fa_seq(fname,leftFlank,rightFlank):
                 # Then reinitialise the identifier and sequence
                 # variables to build up a new record.
                 identifier = line
-                num = re.search('^.+[|(](\d+)[.]+(\d+)',identifier,0)
-                left_flank_begin = int(num.group(1))
-                right_flank_end = int(num.group(2))
+                num = re.search('^>(.+)[|(](\d+)[.]+(\d+)',identifier,0)
+                readID = num.group(1)
+                readID = readID[:-1]
+                left_flank_begin = int(num.group(2))
+                right_flank_end = int(num.group(3))
                 sequence = [] 
             else:
                 # This happens every time a sequence line is encountered.
                 sequence.append(line)
                 #corresponding found transposon will be within one code to find them all positions
-                if(left_flank_begin <= leftFlank and right_flank_end >= rightFlank):
+                if(left_flank_begin <= leftFlank and right_flank_end >= rightFlank and readID == seqID):
                     # Start by yielding the entry that has been built up.
                     yield identifier, sequence
 
@@ -69,7 +70,7 @@ for entry in parse_repMask(repMask_file):
     right_flank_begin = int(entry[2])
 
     #find the corresponding One Code to Find Them All sequence
-    for coordinate in find_fa_seq(fasta_file,left_flank_end,right_flank_begin):
+    for coordinate in find_fa_seq(fasta_file,query_sequence,left_flank_end,right_flank_begin):
         num = re.search('^.+[|(](\d+)[.]+(\d+)',coordinate[0],0)
         left_flank_begin = int(num.group(1))
         right_flank_end = int(num.group(2))
